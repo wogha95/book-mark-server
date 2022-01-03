526b148feae772a2ddf5d90f6ddc84d128f3d2d0
jaeho <wogha95@naver.com>
2022-01-03 19:35:53 +0900

    [refactoring] REST API put + user
    - /api/user
      - put + /user

--- server/routes/index.js
+++ server/routes/index.js
@@ -55,8 +55,8 @@ router.post('/user', async function (req, res, next) {
   }
 });
 
-// localhost:3000/api/updateUser
-router.post('/updateUser', async function (req, res, next) {
+// localhost:3000/api/user
+router.put('/user', async function (req, res, next) {
   console.log('im updateUser');
 
   let email = req.body.email;
